bd252fc791bb00f9e312494915b281be3bbd01fc
soulharbor1111 <soulharbor1111@gmail.com>
2025-12-09 21:09:27 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'black' }}>
+      <h1 style={{ color: 'white', fontSize: '40px' }}>
+        Samadhi Soul Harbor
+      </h1>
+    </div>
+  );
+}
